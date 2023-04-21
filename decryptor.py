44eacaf9a64b0e82df804d8883f368db03af87e7
hagiape <g_chrisf@protonmail.com>
2023-04-21 17:47:01 +0800

    added lists of plain and encrypted vowels

--- decryptor.py
+++ decryptor.py
@@ -1,5 +1,7 @@
 # create decrypting program thru character substitution
 # lists of vowels
+english_vowels = ['a', 'e', 'i', 'o', 'u']
+encrypted_vowels = ['*', '&', '#', '+', '!']
 # ask input from user
 # placeholder variable
 # main code
