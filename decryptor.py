6c483e010bf3928b6a934b40834cb3f54fdc8c67
hagiape <g_chrisf@protonmail.com>
2023-04-21 17:51:26 +0800

    main code: for loop

--- decryptor.py
+++ decryptor.py
@@ -7,4 +7,10 @@ encrypted_text = input('Enter a string to decrypt: ')
 # placeholder variable
 decrypted_text = 'The plain text: ' + '\033[33m' 
 # main code
+for i in range(len(encrypted_text)):
+    if encrypted_text[i] in encrypted_vowels:
+        list_index = encrypted_vowels.index(encrypted_text[i])
+        decrypted_text += english_vowels[list_index]
+    else:
+        decrypted_text += encrypted_text[i]
 # print result
